72baf37b2e54eebaadd9a0e55a0d5d4980998fa2
ZainabOmar <zainab.hammami.zh@gmail.com>
2025-08-31 10:58:03 +0300

    editing data

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,13 +13,14 @@ export const About = (props) => {
             <div className="about-text">
               <h2>عن هدية</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
-              <h3>لماذاستختار هدية؟</h3>
+              <h3>لماذا ستختار هدية؟</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
+                          <li key={
+                            `${d}-${i}`}>{d}</li>
                         ))
                       : "loading"}
                   </ul>
