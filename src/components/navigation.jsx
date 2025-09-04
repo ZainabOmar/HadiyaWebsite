1fbbeedaaaf78ff9470cd971999834eb0c980157
ZainabOmar <zainab.hammami.zh@gmail.com>
2025-09-04 11:39:13 +0300

    editing navbar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            Hadiya || هدية
+            Hadiya <i className="fa fa-gift mx-1"></i> هدية
           </a>{" "}
         </div>
 
