c6c072824e441cab75f0cf8038b9af27bc2f33d5
ZainabOmar <zainab.hammami.zh@gmail.com>
2025-11-09 10:51:11 +0300

    fixing featurescss

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,22 +2,34 @@ import React from "react";
 
 export const Team = (props) => {
   return (
-    <div id="team" className="text-center">
-      <div className="container">
-        <div className="col-md-8 offset-md-2 section-title">
+    <div id="team" className="text-center py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8 offset-md-2 section-title mb-5">
           <h2>فريق العمل</h2>
           <p>
             يتمتّع فريقنا بخبرة واسعة في مجال التصميم الجرافيكي، ويعتمد أحدث تقنيات وأدوات الليزر لإنجاز أعماله بكفاءة وجودة عالية
           </p>
         </div>
-        <div id="row">
+
+        <div className="row justify-content-center">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 offset-md-2 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
+                <div
+                  key={`${d.name}-${i}`}
+                  className="col-10 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center"
+                >
+                  <div className="thumbnail text-center">
+                    <img
+                      src={d.img}
+                      alt={d.name}
+                      className="team-img img-fluid rounded-circle"
+                      style={{
+                        objectFit: "cover",
+                        width: "200px",
+                        height: "200px",
+                      }}
+                    />
+                    <div className="caption mt-3">
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
                     </div>
